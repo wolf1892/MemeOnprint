acae497506a6194fdc2e4460dc34b81b11e18dd0
wolf1892 <manishsingh239@gmail.com>
2021-03-17 00:05:51 +0530

    Mockups are generated
    TODO : Automate buying too

--- notdoneyet.py
+++ notdoneyet.py
@@ -1,6 +1,8 @@
 import base64
 import requests
 import json
+import time
+
 key = ''
 key = base64.b64encode(bytes(key, 'utf-8'))
 keyDecoded = key.decode('ascii')
@@ -8,22 +10,56 @@ header = {'Authorization': 'Basic ' + keyDecoded}
 
 var = 1
 url = 'https://api.printful.com/mockup-generator/create-task/71'
-cont = '{"variant_ids": [4012,4013,4014,4017,4018,4019],"format": "jpg","files": [{"placement": "front","image_url": "https://www.computerhope.com/jargon/r/random-dice.jpg","position": {"area_width": 1800,"area_height": 2400,"width": 1800,"height": 1800,"top": 300,"left": 0}},{"placement": "back","image_url": "https://www.computerhope.com/jargon/r/random-dice.jpg","position": {"area_width": 1800,"area_height": 2400,"width": 1800,"height": 1800,"top": 300,"left": 0}}]}'
-r = requests.get(url,data=cont, headers=header)
-json_object = json.loads(r.content)
-resul1 = json_object["result"]
-print(resul1)
-task = resul1["task_key"]
-status = resul1["status"]
-print(task)
+cont = {
+    "variant_ids" : [4012, 4013, 4014, 4017, 4018, 4019],
+    "format": "jpg",
+    "files" : [
+        {
+            "placement": "front",
+            "image_url": "https://www.computerhope.com/jargon/r/random-dice.jpg",
+            "position": {
+                "area_width": 1800,
+                "area_height": 2400,
+                "width": 1800,
+                "height": 1800,
+                "top": 300,
+                "left": 0
+            }
+        }
+    ]
+}
+y = json.dumps(cont) 
+r = requests.get(url,data=y, headers=header)
+jsonstr = r.content
+
+ds = jsonstr.decode("utf-8")
+json_object = json.loads(ds)
+
+taskkey = json_object['result']['task_key']
+
+
 	
 
 while var == 1 :
-	url2 = 'https://api.printful.com/mockup-generator/task?task_key=z787b740b384eace5cb8e7348ff0693f' 
-	w = requests.get(url2, headers=header)
+	url2 = 'https://api.printful.com/mockup-generator/task?task_key=' 
+	url3 = url2 + taskkey
 	
-	code = json.loads(w.content)
+	w = requests.get(url3, headers=header)
 	
+	getjson = w.content
+
+	ws = getjson.decode("utf-8")
+	object_json = json.loads(ws)
+#	code = json.loads(w.content)
+	status = object_json['result']['status']
+	print(status)
+	time.sleep(15)
+
+	if status == "completed" :
+		break
+
+mockup = object_json['result']['mockups'][0]['mockup_url']
+print(mockup)
 
 	
 
